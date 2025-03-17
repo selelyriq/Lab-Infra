35d7cb2cd9361028a8f1bded28016f0470da68e7
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-03-17 18:00:54 -0400

    work

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+    required_providers {
+        wiz = {
+            source = "AxtonGrams/wiz"
+            version = "1.2.5"
+        }
+        aws = {
+            source = "hashicorp/aws"
+            version = "4.67.0"
+        }
+    }
+}
+
+
---
+++ wiz.tf
@@ -0,0 +1,41 @@
+resource "wiz_cicd_scan_policy" "iac" {
+    name = "IaC Scan Policy"
+    description = "Scan IaC code for security vulnerabilities"
+    iac_params {
+        count_threshold = 3
+        severity_threshold = "CRITICAL"
+        builtin_ignore_tags_enabled = false
+        ignored_rules = [
+            "fd7dd0c6-4953-4b36-bc39-004ec3d870db",
+            "063fb380-9eda-4c08-a31b-9211ee37bd42",
+        ]
+        custom_ignore_tags = {
+            key = "testkey1"
+            value = "testval1"
+            ignore_all_rules = false
+            rule_ids = [
+                "063fb380-9eda-4c08-a31b-9211ee37bd42",
+            ]
+        }
+    }
+}
+
+resource "wiz_cicd_scan_policy" "vulnerabilities" {
+    name = "Vulnerabilities Scan Policy"
+    description = "Scan for vulnerabilities in the code"
+    disk_vulnerabilities_params {
+        ignore_unfixed = true
+        package_allow_list = [
+            "lsof",
+            "sudo",
+            "apt",
+        ]
+        package_count_threshold = 3
+        severity_threshold = "LOW"
+    }
+}
+
+resource "wiz_cicd_scan_policy" "secrets" {
+    name = "Secrets Scan Policy"
+    description = "Scan for secrets in the code"
+}
