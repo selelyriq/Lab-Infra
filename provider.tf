0f4fc6da16733955975324501489583bd441e81e
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-03-17 19:41:52 -0400

    format

--- provider.tf
+++ provider.tf
@@ -1,14 +1,14 @@
 terraform {
-    required_providers {
-        wiz = {
-            source = "AxtonGrams/wiz"
-            version = "1.2.5"
-        }
-        aws = {
-            source = "hashicorp/aws"
-            version = "4.67.0"
-        }
+  required_providers {
+    wiz = {
+      source  = "AxtonGrams/wiz"
+      version = "1.2.5"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
 }
 
 
