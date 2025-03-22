a93f4c58e284c725bb2fb29fc07734eadf46daab
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-03-21 20:00:26 -0400

    remove insecure resources from main.tf, update provider configuration in provider.tf, and simplify GitHub Actions workflow

--- provider.tf
+++ provider.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    wiz = {
-      source  = "AxtonGrams/wiz"
-      version = "1.2.5"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "4.67.0"
