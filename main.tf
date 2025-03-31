f0edf6f56d770481a52e64f745b05aafe650ecbc
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-03-30 21:54:55 -0400

    modules broken but lol

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 
 
 module "ec2" {
-  source        = "https://github.com/selelyriq/TF-EC2.git"
-  version       = "1.0.0"
+  source        = "https://github.com/selelyriq/TF-EC2?ref=1.0.0"
   instance_type = var.instance_type
   ami           = var.ami
   user_data     = var.user_data
