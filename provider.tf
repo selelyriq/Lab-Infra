c9020fe4b839c3ad4fefef446142e823aed89fb7
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-03-30 22:15:13 -0400

    tf provider fix

--- provider.tf
+++ provider.tf
@@ -1,12 +1,8 @@
 terraform {
   required_providers {
-    terraform = {
-      source  = "hashicorp/terraform"
-      version = "~> 1.8.0"
-    }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 4.0"
     }
   }
 }
