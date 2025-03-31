79984163a79c3e221e81edbea3244748f5f5230d
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-03-30 22:00:18 -0400

    tagging fix

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
-
-
 module "ec2" {
-  source        = "https://github.com/selelyriq/TF-EC2?ref=1.0.0"
+  source        = "git::https://github.com/selelyriq/TF-EC2.git?ref=a88fb52782745d7a92228ddee1f5f7acd5831f51"
   instance_type = var.instance_type
-  ami           = var.ami
+  ami           = var.ami   
   user_data     = var.user_data
   tags          = var.tags
 }
