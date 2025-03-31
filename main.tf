3a146fedd034a7b3bed41c5188728529bdf1d347
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-03-30 22:03:37 -0400

    fmt

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "ec2" {
   source        = "git::https://github.com/selelyriq/TF-EC2.git?ref=a88fb52782745d7a92228ddee1f5f7acd5831f51"
   instance_type = var.instance_type
-  ami           = var.ami   
+  ami           = var.ami
   user_data     = var.user_data
   tags          = var.tags
 }
